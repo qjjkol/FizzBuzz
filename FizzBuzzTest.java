ac9dcecfb231a5b56edd18954c0dafaa25d44383
qjjkol <safaejaa134@gmail.com>
2024-12-29 16:57:06 +0100

    cycle 1

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -2,7 +2,7 @@ package com.controle.tdd;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FizzBuzzTest { 
+public class FizzBuzzTest {
  @Test
     void testFizzBuzzReturnsFizzForMultipleOf3() {
         //Given
